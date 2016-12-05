0e7467a0e739a0ff1b0380828874f405d55e55d8
Nazarko <Nazarko0o@i.ua>
2016-12-05 18:01:39 +0200

    Nazarko

---
+++ EmployeeManipulation/EmployeeManipulation/src/main/java/TeamWork/EmployeeManipulation/Main.java
@@ -0,0 +1,24 @@
+package TeamWork.EmployeeManipulation;
+
+import java.io.FileInputStream;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.fasterxml.jackson.core.JsonParseException;
+
+public class Main {
+
+	public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException {
+		InputStream input = new FileInputStream("Employee.json");
+		ObjectMapper objectMapper = new ObjectMapper();
+		TypeFactory typeFactory = TypeFactory.defaultInstance();
+		List<User> person = objectMapper.readValue(input,typeFactory.constructCollectionType(ArrayList.class, User.class));
+
+}
+}
---
+++ EmployeeManipulation/EmployeeManipulation/src/main/java/TeamWork/EmployeeManipulation/User.java
@@ -0,0 +1,28 @@
+package TeamWork.EmployeeManipulation;
+
+public class User {
+
+	private String name;
+	private String position;
+	private Integer salary;
+	
+	public String getName()
+	{
+	return name;	
+	}
+	 public String getPosition() {
+		return position;
+	}
+	 public Integer getSalary() {
+		return salary;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	public void setSalary(Integer salary) {
+		this.salary = salary;
+	}
+}
